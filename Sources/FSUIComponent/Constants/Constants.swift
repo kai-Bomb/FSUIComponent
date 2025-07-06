5d2a4fecdb82a7fb753d9fe07d8225d70cac4fad
kai-Bomb <katsumasa.watanabe@zozo.com>
2025-07-06 15:19:06 +0900

    feat: Added public

--- Sources/FSUIComponent/Constants/Constants.swift
+++ Sources/FSUIComponent/Constants/Constants.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 public struct Constants {
-    static var fsXXSmall: CGFloat { 2 }
-    static var fsXSmall: CGFloat { 4 }
-    static var fsSmall: CGFloat { 8 }
-    static var fsStandard: CGFloat { 16 }
-    static var fsLarge: CGFloat { 24 }
-    static var fsXLarge: CGFloat { 32 }
-    static var fsXXLarge: CGFloat { 40 }
-    static var fsXXXLarge: CGFloat { 48 }
+    public static var fsXXSmall: CGFloat { 2 }
+    public static var fsXSmall: CGFloat { 4 }
+    public static var fsSmall: CGFloat { 8 }
+    public static var fsStandard: CGFloat { 16 }
+    public static var fsLarge: CGFloat { 24 }
+    public static var fsXLarge: CGFloat { 32 }
+    public static var fsXXLarge: CGFloat { 40 }
+    public static var fsXXXLarge: CGFloat { 48 }
 }
 
 public extension CGFloat {
